269330b83135a04ceaac9f84d8b71830d2339535
Aronno Apu <aronnoapucse@gmail.com>
2019-07-17 12:16:46 +0600

    first commit

---
+++ apu.java
@@ -0,0 +1,11 @@
+public class apu 
+{
+    public static void main(String[] args) 
+    {
+        
+        int first = 10;
+        int second = 20;
+        int sum = first + second;
+        System.out.println("The sum is: " + sum);
+    }
+}
