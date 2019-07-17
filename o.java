e51a9b5d6cfa24acf001a7859dab17d66a5fdd39
Aronno Apu <aronnoapucse@gmail.com>
2019-07-17 13:25:07 +0600

    addd

---
+++ o.java
@@ -0,0 +1,11 @@
+public class o 
+{
+    public static void main(String[] args) 
+    {
+        
+        int first = 20;
+        int second = 30;
+        int sum = first + second;
+        System.out.println("The sum is: " + sum);
+    }
+}
